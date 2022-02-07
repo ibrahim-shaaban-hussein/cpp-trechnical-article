a69f7a3887e802413e5a371ef1cc80ba163ef2c6
Ibrahim S. Hussein <ibrahim_shabaan@yahoo.com>
2022-02-07 21:14:27 +0200

    Test first commit

---
+++ tuple2.py
@@ -0,0 +1,9 @@
+#thistuple = ("apple", "banana", "cherry", "apple")
+#print(len(thistuple))
+
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
---
+++ tuple4.py
@@ -0,0 +1,5 @@
+#thistuple = ("apple", "banana", "cherry")
+#print(thistuple[-1])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
